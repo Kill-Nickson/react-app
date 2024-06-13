8ac23598eee46d05b53b466a00fa9e3df98a71be
Kill-Nickson <gleb.basov.1998@gmail.com>
2024-06-13 14:11:32 +0300

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
+import { PersistGate } from "redux-persist/integration/react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import { ThemeProvider } from "@material-tailwind/react";
+import { persistor, store } from "@store";
+import { Layout, Main, NotFound } from "@pages";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+
+const routes = createBrowserRouter([
+  {
+    // path: ROUTE.ABSOLUTE,
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {/* <I18nextProvider i18n={i18n}> */}
+          <RouterProvider router={routes} />
+        {/* </I18nextProvider> */}
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
---
+++ src/pages/Main.tsx
@@ -0,0 +1,10 @@
+
+const Main = () => {
+  return (
+    <div className="mx-auto w-[300px] flex justify-center items-center gap-4">
+      Main
+    </div>
+  );
+};
+
+export default Main;
